0a248694644e7897a54cdb77e426575f7700a1e4
MujuniEli <elijahmujuni@gmail.com>
2023-03-10 23:12:19 +0300

    upload reverse array func

--- chapter4/reverseArray.js
+++ chapter4/reverseArray.js
@@ -3,3 +3,11 @@ The first, reverseArray, takes an array as argument and produces a new array tha
 The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. 
 Neither may use the standard reverse method. */
 
+function reverseArray(array) {
+    let reversedArray = [];
+    for(let i = array.length - 1; i >= 0; i--){
+        reversedArray.push(array[i])
+    }
+    return reversedArray;
+}
+
